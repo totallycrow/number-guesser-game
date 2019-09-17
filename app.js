0a4b6b1f8a47854bb9acfc9d187b57e66f2407ca
Adrian Gajda <adrian.gajda@hotmail.com>
2019-09-17 22:08:25 +0100

    Add validation and winning case

---
+++ app.js
@@ -0,0 +1,51 @@
+// Game Values
+
+let min = 1,
+    max = 10,
+    winningNum = 2,
+    guessesLeft = 3;
+
+// UI Elements
+
+const game = document.querySelector('#game'),
+    minNum = document.querySelector('.min-num'),
+    maxNum = document.querySelector('.max-num'),
+    guessBtn = document.querySelector('#guess-btn'),
+    guessInput = document.querySelector('#guess-input'),
+    message = document.querySelector('.message');
+
+// Assing UI min and max
+
+minNum.textContent = min;
+maxNum.textContent = max;
+
+// Listen for guess
+guessBtn.addEventListener('click', function () {
+    let guess = parseInt(guessInput.value);
+
+    // Validate
+    if (isNaN(guess) || guess < min || guess > max) {
+        setMessage(`Please enter a number between ${min} and ${max}`, 'red');
+    }
+
+    // Check if won
+    if (guess === winningNum) {
+        // Disable input
+        guessInput.disabled = true;
+
+        // Change border color
+        guessInput.style.borderColor = 'green';
+
+        // Set Message
+        setMessage(`${winningNum} is correct, You Win!`, 'green');
+
+    } else {
+
+    }
+})
+
+// Set Message
+function setMessage(msg, color) {
+    message.style.color = color;
+    message.textContent = msg;
+}
